4e634bc5b22dd1eab6e75a44e91562e759b247a4
Edu Caribe <edu.caribe@hotmail.com>
2021-10-05 20:10:24 -0300

    feat: help box experiences section and structure margin fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'caribeedu-me';
+    /**  */
+    public expHelpActive: boolean = false;
+
+    /**
+     *
+     */
+    public changeHelpState(): void {
+        this.expHelpActive = !this.expHelpActive;
+    }
 }
