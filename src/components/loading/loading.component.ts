949cc6ac994b62baa879db398afebe089894a42d
Edu Caribe <edu.caribe@hotmail.com>
2021-10-19 17:23:56 -0300

    refactor: split loading state listener into method

--- src/components/loading/loading.component.ts
+++ src/components/loading/loading.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { LoadingService } from 'src/services';
 import { ELoadingState } from 'src/enums';
@@ -9,11 +10,13 @@ import { ELoadingState } from 'src/enums';
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss']
 })
-export class LoadingComponent implements OnInit {
+export class LoadingComponent implements OnInit, OnDestroy {
     /** */
-    public openingTimeout: NodeJS.Timeout | null = null;
+    public activeTimeout: NodeJS.Timeout | null = null;
     /** */
     public active$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+    /** */
+    public destroy$: Subject<void> = new Subject<void>();
     /** */
     public shouldClose: boolean = false;
 
@@ -21,27 +24,40 @@ export class LoadingComponent implements OnInit {
         public loadingService: LoadingService
     ) { }
 
+    /**  */
     public ngOnInit(): void {
-        this.loadingService.state$.subscribe((state: ELoadingState) => {
-            if (state === ELoadingState.CLOSED) {
-                if (this.openingTimeout) {
-                    this.shouldClose = true;
-                }
-                else {
-                    this.active$.next(false);
-                }
-            }
-            else if (state === ELoadingState.OPEN) {
-                this.active$.next(true);
-                this.shouldClose = false;
+        this.handleStateChanges();
+    }
 
-                this.openingTimeout = setTimeout(() => {
-                    if (this.shouldClose) {
+    /** */
+    public handleStateChanges(): void {
+        this.loadingService.state$
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((state: ELoadingState) => {
+                if (state === ELoadingState.CLOSED) {
+                    if (this.activeTimeout) {
+                        this.shouldClose = true;
+                    }
+                    else {
                         this.active$.next(false);
                     }
-                    this.openingTimeout = null;
-                }, 8000);
-            }
+                }
+                else if (state === ELoadingState.OPEN) {
+                    this.active$.next(true);
+                    this.shouldClose = false;
+
+                    this.activeTimeout = setTimeout(() => {
+                        if (this.shouldClose) {
+                            this.active$.next(false);
+                        }
+                        this.activeTimeout = null;
+                    }, 8000);
+                }
         });
+    }
+
+    /** */
+    public ngOnDestroy(): void {
+        this.destroy$.next();
     }
 }
